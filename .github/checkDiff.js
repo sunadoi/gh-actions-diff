72ad676efd075a86963199322e355a61a5863517
sunadoi <y.sunadoi@gmail.com>
2022-10-03 11:50:15 +0900

    delete

--- .github/checkDiff.js
+++ .github/checkDiff.js
@@ -1,8 +1,25 @@
 module.exports = ({ github, context }) => {
+  const { data: comments } = await github.rest.issues.listComments({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number,
+  })
+  const botComment = comments.find(comment => {
+    return comment.user.type === 'Bot' && comment.body.includes('body')
+  });
+
+  if (botComment) {
+    github.rest.issues.deleteComment({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      comment_id: botComment.id,
+    });
+  }
+
   github.rest.issues.createComment({
     issue_number: context.issue.number,
     owner: context.repo.owner,
     repo: context.repo.repo,
-    body: "comment body",
+    body: "comment",
   });
 };
