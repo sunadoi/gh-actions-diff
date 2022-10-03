285b1d0197c4e57dd53ca1eaefe3f9215b57fa6c
sunadoi <y.sunadoi@gmail.com>
2022-10-03 21:44:46 +0900

    fhore

--- .github/checkDiff.js
+++ .github/checkDiff.js
@@ -1,14 +1,14 @@
-const body = `
-Uncommitted changes were detected after runnning <code>generate</code> command.
-Please run <code>pnpm run generate:locales</code> or <code>pnpm run generate:api-docs</code> to generate/update the related files, and commit them.
-`;
-
 module.exports = async ({ github, context, isSuccess }) => {
   const { data: comments } = await github.rest.issues.listComments({
     owner: context.repo.owner,
     repo: context.repo.repo,
     issue_number: context.issue.number,
   });
+
+  const body = `
+  Uncommitted changes were detected after runnning <code>generate</code> command.
+  Please run <code>pnpm run generate:locales</code> or <code>pnpm run generate:api-docs</code> to generate/update the related files, and commit them.
+  `;
 
   const botComment = comments.find(
     (comment) => comment.user.type === "Bot" && comment.body.includes(body)
